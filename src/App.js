db08b2254d24047938983be400e6f16981e980bd
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-30 06:07:59 +0100

    Commit changes re routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import Login from "./components/views/login/Login";
+import Signup from "./components/views/signup/Signup";
+import Dashboard from "./components/views/dashboard/Dashboard";
+import Profile from "./components/views/profile/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route exact path="/" render={routeProps => <Signup {...routeProps} />} />
+      <Route
+        exact
+        path="/login"
+        render={routeProps => <Login {...routeProps} />}
+      />
+      <Route
+        exact
+        path="/dashboard"
+        render={routeProps => <Dashboard {...routeProps} />}
+      />
+      <Route
+        exact
+        path="/profile"
+        render={routeProps => <Profile {...routeProps} />}
+      />
+    </>
   );
 }
 
