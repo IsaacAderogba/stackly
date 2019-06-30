deb69749c8cbd12662bb2d6c3d090da04e9facc0
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-30 05:59:41 +0100

    Scaffold core components

---
+++ src/components/~reusables/variables/colors.js
@@ -0,0 +1,7 @@
+//====== COLOR VARIABLES ======//
+export const primary = '#52D2BC';
+export const secondary = '#f7f8f8';
+export const text = '#838E9E';
+export const background = "#16161a";
+export const alt_background = "#E6EBF9";
+export const white = '#FFFFFF';
